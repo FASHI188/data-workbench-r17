c835f42e46615b2b49147d3b46596bca9373f69a
FASHI188 <hanzhichao818@gmail.com>
2026-08-02 12:56:05 +0800

    Remove superseded V17.26 promotion wrapper

--- scripts/promote_stage3_s3g1j_v17_26_full_shards_v2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from __future__ import annotations
-
-from promote_stage3_s3g1j_v17_26_full_shards_v3 import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
